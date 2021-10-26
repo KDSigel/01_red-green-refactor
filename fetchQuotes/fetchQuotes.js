b96f236d2a5274cfcc649f18d8321fbc0a3a50f5
Karl Sigel <heykarl@gmail.com>
2021-10-26 12:14:48 -0700

    couldn't get any assistance, tried a lot of things, did research on internet, commiting assingment unfinished

--- fetchQuotes/fetchQuotes.js
+++ fetchQuotes/fetchQuotes.js
@@ -3,11 +3,17 @@ const fetch = require('cross-fetch');
 const fetchQuotes = async (number) => {
 
   const results = await fetch(`https://futuramaapi.herokuapp.com/api/quotes/${number}`);
-  const body = await results.json();
-  return {
-    name: (body.character),
-    text: (body.quote),
-    image: (body.image)
-  };
+  // .then(results => results.json());
+  // .then(result => result.Body.json(results));
+  //   const body = await results.json();
+  results.then(response => response.json())
+    .then(body => {
+
+      return {
+        name: (body.character),
+        text: (body.quote),
+        image: (body.image)
+      };
+    });
 };
 module.exports = fetchQuotes;
